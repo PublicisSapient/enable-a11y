27e2ec4638788b5b080a6cec4cf34e763a1c4e76
Zoltan Hawryluk <zoltan.dulac@gmail.com>
2022-08-12 15:01:40 -0400

    Started Hamburger menu tests.

--- js/test/test-config.js
+++ js/test/test-config.js
@@ -4,7 +4,9 @@ jest.setTimeout(10000);
 const config = {
   "BASE_URL": `http://localhost:8888`,
   "KEYPRESS_TIMEOUT": 250,
-  "KEYPRESS_FAST_TIMEOUT": 100
+  "KEYPRESS_FAST_TIMEOUT": 100,
+  "DESKTOP_WIDTH": 1024,
+  "DESKTOP_HEIGHT": 768
 };
 
 export default config;
--- js/test/test-helpers.js
+++ js/test/test-helpers.js
@@ -1,8 +1,31 @@
 'use strict';
 
+import config from './test-config.js';
+
+
 const testHelpers = new function () {
   this.isElementHidden = function (el) {
     return (el.offsetParent === null)
+  }
+
+  this.getDesktopBrowser = async function (config, puppeteer) {
+    try {
+      const browser = await puppeteer.launch({
+        //headless: false, // The browser is visible
+        ignoreHTTPSErrors: true,
+        args: [`--window-size=${config.DESKTOP_WIDTH},${config.DESKTOP_HEIGHT}`],
+        defaultViewport: {
+          width: config.DESKTOP_WIDTH,
+          height: config.DESKTOP_HEIGHT
+        }
+      });
+
+      return browser;
+    } catch (ex) {
+      console.log('ex', ex);
+      return ex;
+    }
+    
   }
 }
 
