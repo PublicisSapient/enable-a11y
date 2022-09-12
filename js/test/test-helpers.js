7f4e02b3d6a145b1e7a6125578e64fac492bb48e
Zoltan Hawryluk <zoltan.dulac@gmail.com>
2022-09-12 18:08:21 -0400

    Remove unneeded method in testHelpers

--- js/test/test-helpers.js
+++ js/test/test-helpers.js
@@ -124,38 +124,6 @@ const testHelpers = new function () {
     }); */
   }
 
-  this.waitTillHTMLRendered = async (page, timeout = 30000) => {
-    const checkDurationMsecs = 1000;
-    const maxChecks = timeout / checkDurationMsecs;
-    let lastHTMLSize = 0;
-    let checkCounts = 1;
-    let countStableSizeIterations = 0;
-    const minStableSizeIterations = 3;
-  
-    while(checkCounts++ <= maxChecks){
-      let html = await page.content();
-      let currentHTMLSize = html.length; 
-  
-      let bodyHTMLSize = await page.evaluate(() => document.body.innerHTML.length);
-  
-      console.log('last: ', lastHTMLSize, ' <> curr: ', currentHTMLSize, " body html size: ", bodyHTMLSize);
-  
-      if(lastHTMLSize != 0 && currentHTMLSize == lastHTMLSize) 
-        countStableSizeIterations++;
-      else 
-        countStableSizeIterations = 0; //reset the counter
-  
-      if(countStableSizeIterations >= minStableSizeIterations) {
-        console.log("Page rendered fully..");
-        break;
-      }
-  
-      lastHTMLSize = currentHTMLSize;
-      await page.waitForTimeout(checkDurationMsecs);
-    }  
-  };
-  
-
 }
 
 export default testHelpers;
