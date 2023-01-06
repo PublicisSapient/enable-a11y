913398a9e37261412d9e334a4a96e4db488d6056
Zoltan Hawryluk <zoltan.dulac@gmail.com>
2023-01-06 12:20:36 -0500

    Adding Bookmarks page.

--- content/body/bookmarklets.php
+++ content/body/bookmarklets.php
@@ -5,53 +5,67 @@
 
 <h2>How To Install Bookmarklets For Your Browser</h2>
 
-<p>The easiest, most straight-forward way to install any of the bookmarklets in your browser is to drag them into your
-  Bookmarks Toolbar.</p>
+<p>The easiest, most straight-forward way to install any of the bookmarklets in your browser is to make your Bookmarks
+  Toolbar visible and dragging them in there.</p>
 
 <ol>
-  <li>First, make sure your Bookmarks Toolbar is visible:
+  <li>
+    <details class="enable-drawer">
+      <summary class="enable-drawer__button">
 
-    <table>
-      <thead>
-        <tr>
-          <th scope="col">Browser</th>
-          <th scope="col">Instructions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">Chrome for Windows and Linux</th>
-          <td>Press Control+Shift+B on your keyboard.</td>
+        Read how you can make your browser's Bookmarks Toolbar Visible.
 
-        </tr>
-        <tr>
-          <th scope="row">Safari and Chrome for Mac</th>
-          <td>
-            <p>Press ⌘+Shift+B on your keyboard</p>
-          </td>
-        </tr>
-        <tr>
-          <th scope="row">Firefox</th>
-          <td>
-            <p>Why does Mozilla make this so difficult?!?!</p>
-            <ol>
-              <li>In the navigation toolbar, go to the rightmost icon (screen reader users will hear it being refered to
-                as the "Firefox" button. Sighted users will see it as a hamburger menu at the right most edge of the
-                naviation toolbar, as shown in the screenshot below:<br class="break--with-space" />
-                <img src="images/bookmarklets/firefox-menu.png" alt="" />
-              </li>
-              <li>Choose the Bookmarks item, as shown in this screenshot: <br class="break--with-space" />
-                <img src="images/bookmarklets/firefox-menu-1.png" alt="" />
-              </li>
-              <li>In the new menu that appears, choose <strong>Show Bookmarks Toolbar</strong>, as shown in the
-                screenshot below: <br class="break--with-space" />
-                <img src="images/bookmarklets/firefox-menu-2.png" alt="" />
-              </li>
-            </ol>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+      </summary>
+      <div class="content">
+
+        <table>
+          <thead>
+            <tr>
+              <th scope="col">Browser</th>
+              <th scope="col">Instructions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">Chrome for Windows and Linux</th>
+              <td>Press Control+Shift+B on your keyboard.</td>
+
+            </tr>
+            <tr>
+              <th scope="row">Safari and Chrome for Mac</th>
+              <td>
+                <p>Press ⌘+Shift+B on your keyboard</p>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">Firefox</th>
+              <td>
+                <p>Why does Mozilla make this so difficult?!?!</p>
+                <ol>
+                  <li>In the navigation toolbar, go to the rightmost icon (screen reader users will hear it being
+                    refered to
+                    as the "Firefox" button. Sighted users will see it as a hamburger menu at the right most edge of the
+                    naviation toolbar, as shown in the screenshot below:<br class="break--with-space" />
+                    <img src="images/bookmarklets/firefox-menu.png" alt="" />
+                  </li>
+                  <li>Choose the Bookmarks item, as shown in this screenshot: <br class="break--with-space" />
+                    <img src="images/bookmarklets/firefox-menu-1.png" alt="" />
+                  </li>
+                  <li>In the new menu that appears, choose <strong>Show Bookmarks Toolbar</strong>, as shown in the
+                    screenshot below: <br class="break--with-space" />
+                    <img src="images/bookmarklets/firefox-menu-2.png" alt="" />
+                  </li>
+                </ol>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+      </div>
+    </details>
+
+
+
   </li>
   <li>Next, drag any of the bookmarklet links below into the Bookmarks Toolbar in your web browser.</li>
   <li>That's it! Whenever you want to use any of the bookmarklets on your webpage, just click it!</li>
@@ -78,7 +92,9 @@
 <h3>What Has Focus?</h3>
 
 <p>
-  I wrote this bookmarklet when I was testing a page that was allowing keyboard focus to be applied to visually hidden elements on the page.  When you use the bookmarklet, every time you tab into an interactive element, its HTML appears in the console.  I use it all the time and find it super useful, so I include it here.
+  I wrote this bookmarklet when I was testing a page that was allowing keyboard focus to be applied to visually hidden
+  elements on the page. When you use the bookmarklet, every time you tab into an interactive element, its HTML appears
+  in the console. I use it all the time and find it super useful, so I include it here.
 </p>
 
 <a class="bookmarklet"
@@ -87,10 +103,14 @@
 
 <h2>Other Accessibility Related Bookmarklets</h2>
 
-<p>There are, of course, many other places to get a11y related bookmarklets.  Here are a few we know about.</p>
+<p>There are, of course, many other places to get a11y related bookmarklets. Here are a few we know about.</p>
 
 <ul>
-  <li><a href="https://pauljadam.com/bookmarklets/">Paul J. Adams' JavaScript Bookmarklets for Accessibility Testing</a></li>
-  <li><a href="https://whatsock.github.io/visual-aria/github-bookmarklet/visual-aria.htm">The Visual ARIA Bookmarklet</a> allows engineers, testers, educators, and students to physically observe ARIA usage within web technologies, including ARIA 1.1 structural, live region, and widget roles, proper nesting and focus management, plus requisite and optional supporting attributes to aid in development.</li>
+  <li><a href="https://pauljadam.com/bookmarklets/">Paul J. Adams' JavaScript Bookmarklets for Accessibility Testing</a>
+  </li>
+  <li><a href="https://whatsock.github.io/visual-aria/github-bookmarklet/visual-aria.htm">The Visual ARIA
+      Bookmarklet</a> allows engineers, testers, educators, and students to physically observe ARIA usage within web
+    technologies, including ARIA 1.1 structural, live region, and widget roles, proper nesting and focus management,
+    plus requisite and optional supporting attributes to aid in development.</li>
   <li><a href="text-spacing.php">Text Spacing Bookmarklet by Steve Faulkner</a>.</li>
 </ul>
