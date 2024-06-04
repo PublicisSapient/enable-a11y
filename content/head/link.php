3987b1753ea0cd15d087e3951610c2063d07fbe0
Alison Hall <alison.hall@publicissapient.com>
2024-06-04 16:00:16 -0400

    Fix the link comment format

--- content/head/link.php
+++ content/head/link.php
@@ -1,7 +1,5 @@
 <link id="link-css" rel="stylesheet" type="text/css" href="css/link.css" >
 
 <?php // since we are using an example from this page, we need to include the stylesheet from it as well.
-// since we are using an example from this page, we need to include the stylesheet from it as well.
-// since we are using an example from this page, we need to include the stylesheet from it as well.
 include "hero-image-text-resize.php";
 ?>
