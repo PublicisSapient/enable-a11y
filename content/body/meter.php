f0d49acd1387b68f7e7a6634cfec615ef3924639
akr3081 <adamr34@gmail.com>
2024-06-10 11:34:44 -0400

    Added css attribute selector for aria example

--- content/body/meter.php
+++ content/body/meter.php
@@ -22,13 +22,13 @@
 
   <div class="meter-container">
     <p aria-hidden="true">C:</p>
-    <meter aria-label="Disk C" min="0" max="100" optimum="0" low="20" high="80"  value="20"></meter>
+    <meter aria-label="Disk C" value="0.2" min="0" max="1" optimum="0" low="0.2" high="0.8"></meter>
   
     <p aria-hidden="true">D:</p>
-    <meter aria-label="Disk D" min="0" max="100" optimum="0" low="20" high="80" value="90"></meter>
+    <meter aria-label="Disk D" value="0.9" min="0" max="1" optimum="0" low="0.2" high="0.8"></meter>
 
     <p aria-hidden="true">E:</p>
-    <meter aria-label="Disk E" min="0" max="100" optimum="0" low="20" high="80" value="60"></meter>
+    <meter aria-label="Disk E" value="0.6" min="0" max="1" optimum="0" low="0.2" high="0.8"></meter>
   </div>
 </div>
 
@@ -42,12 +42,12 @@
             "notes": "Set the <code>min</code> and <code>max</code> values to the min and max values of what your meter bar is measuring. <code>value</code> is the current value. Setting the <code>low</code>, <code>high</code>, and <code>optimum</code> fields will result in browser specific colors."
         },
         {
-            "label": "Use <code>aria-label</code> field to announce the name of each meter.",
-            "highlight": "aria-label",
+          "label": "Add the <code>aria-label</code> attribute to announce the name of each meter.",
+          "highlight": "aria-label",
             "notes": ""
         },
         {
-            "label": "Use <code>aria-hidden</code> field on any visual labels to prevent redundant items being read out.",
+            "label": "Apply <code>aria-hidden</code> to any visual labels to prevent redundant items being read out.",
             "highlight": "%OPENCLOSECONTENTTAG%p",
             "notes": ""
         }
@@ -68,51 +68,33 @@
     <p aria-hidden="true">C:</p>
     <div
       class="meter"
-      role="meter" 
+      role="meter"
       aria-label="Disk C"
-      aria-valuenow="20"
-      value="20"
+      aria-valuenow="0.2"
       aria-valuemin="0"
-      min="0" 
-      aria-valuemax="100"
-      max="100" 
-      optimum="0" 
-      low="20" 
-      high="80"  
+      aria-valuemax="1"
     >
     </div>
   
     <p aria-hidden="true">D:</p>
     <div
       class="meter"
-      role="meter" 
+      role="meter"
       aria-label="Disk D"
-      aria-valuenow="90"
-      value="90"
+      aria-valuenow="0.9"
       aria-valuemin="0"
-      min="0" 
-      aria-valuemax="100"
-      max="100" 
-      optimum="0" 
-      low="20" 
-      high="80"  
+      aria-valuemax="1"
     >
     </div>
 
     <p aria-hidden="true">E:</p>
     <div
       class="meter"
-      role="meter" 
+      role="meter"
       aria-label="Disk E"
-      aria-valuenow="60"
-      value="60"
+      aria-valuenow="0.6"
       aria-valuemin="0"
-      min="0" 
-      aria-valuemax="100"
-      max="100" 
-      optimum="0" 
-      low="20" 
-      high="80"  
+      aria-valuemax="1"
     >
     </div>
   </div>
@@ -128,9 +110,19 @@
             "notes": ""
         },
         {
-            "label": "Set the associated values for min, max, and value. You will also want to set the associated aria values.",
-            "highlight": "aria-valuenow ||| aria-valuemin ||| aria-valuemax ||| value ||| min ||| max ",
-            "notes": "These will be <code>aria-valuemin</code>, <code>aria-valuemax</code>, and <code>aria-valuenow</code>."
+            "label": "Set the associated values for min, max, and value using the associated aria values.",
+            "highlight": "aria-valuenow ||| aria-valuemin ||| aria-valuemax",
+            "notes": "These will be <code>aria-valuemin</code>, <code>aria-valuemax</code>, and <code>aria-valuenow</code>. Using aria values will ensure the screen reader announces these correctly."
+        },
+        {
+            "label": "Add the <code>aria-label</code> attribute to announce the name of each meter.",
+            "highlight": "aria-label",
+            "notes": ""
+        },
+        {
+          "label": "Apply <code>aria-hidden</code> to any visual labels to prevent redundant items being read out.",
+          "highlight": "%OPENCLOSECONTENTTAG%p",
+          "notes": ""
         }
     ]
 }
