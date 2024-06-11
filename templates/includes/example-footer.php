2230fc91223eb0f7068ed9dea7de1bc05dd9dbda
Matt Slavin <matthew.slavin@publicissapient.com>
2024-06-11 09:56:49 -0400

    Update nav to include sticky header and move content to one line

--- templates/includes/example-footer.php
+++ templates/includes/example-footer.php
@@ -1,5 +1,5 @@
     <footer aria-label="Copyright Information">
-            
+        <?php include "includes/about-enable-nav.php"; ?>   
         Enable is a labour of love created by
         <a href="https://useragentman.com">Zoltan Hawryluk</a>.  It has been released as open
         source so hopefully others can learn from it.  This content is covered by the the <a href="https://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International Licence</a>.
