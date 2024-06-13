a146fba54de4f368f70b070c27fb25560c04ea2f
Alison Hall <alison.hall@publicissapient.com>
2024-06-13 13:54:39 -0400

    Add additional helper functions

--- js/modules/helpers.js
+++ js/modules/helpers.js
@@ -1,6 +1,6 @@
 'use strict'
 
-function scrollToEl(el) {
+function scrollToEl(el, offset = 0) {
     /*
      * The setTimeout is here to ensure the focused elements coordinates are accurate
      */
@@ -13,10 +13,22 @@ function scrollToEl(el) {
             elTop = rect.top + scrollTop,
             elLeft = rect.left + scrollLeft;
         window.scrollTo({
-            top: elTop - 200,
+            top: elTop - offset,
             left: elLeft,
         });
     }, 100);
+}
+
+function focusDeepLink() {
+    const { hash } = window.location;
+
+    if (hash !== '') {
+        const elToFocus = document.querySelector(hash);
+        if (elToFocus) {
+            elToFocus.focus();
+            scrollToEl(elToFocus, 100);
+        }
+    }
 }
 
 function addMissingIDToHeading(headingElement, headingIndex = 0) {
@@ -28,6 +40,69 @@ function addMissingIDToHeading(headingElement, headingIndex = 0) {
             headingElement.id = `${innerTextId}-${headingIndex}`;
         } else {
             headingElement.id = `${innerTextId}`;
+        }
+    }
+}
+
+function findImagesNextToHeading(headingId, className) {
+    const heading = document.getElementById(headingId);
+    let images = '';
+
+    if (heading) {
+        let nextElement = heading.nextElementSibling;
+
+        // Iterate over siblings with the specified class name
+        while (nextElement && nextElement.classList.contains(className)) {
+            const imgElements = nextElement.querySelectorAll('img');
+
+            // Append attributes of img elements to the images string
+            imgElements.forEach((img) => {
+                const attributes = Array.from(img.attributes)
+                    .map((attr) => {
+                        return attr.name !== 'class'
+                            ? `${attr.name}="${attr.value}"`
+                            : ((attr.value += ' enable-stats__heading-icon'),
+                              `${attr.name}="${attr.value}"`);
+                    })
+                    .join(' ');
+                images += `<img ${attributes}>`;
+            });
+            // Move to the next sibling
+            nextElement = nextElement.nextElementSibling;
+        }
+    }
+    return images;
+}
+
+function createPermalinksForHeading(headingElement, headingIndex, includeImages) {
+    // Make sure the heading has an ID
+    addMissingIDToHeading(headingElement, headingIndex);
+
+    // If the heading doesn't have a tabindex, add one.
+    if (headingElement.getAttribute('tabIndex') === null) {
+        headingElement.setAttribute('tabIndex', '-1');
+    }
+
+    // now, let's put a link tag inside the heading so we can deeplink to it easily
+    if (
+        headingElement.nodeName !== 'H1' &&
+        headingElement.getAttribute('role') !== 'heading'
+    ) {
+        // now, let's put a link tag inside the heading so we can deeplink to it easily
+        headingElement.innerHTML = `<a class="heading__deeplink" href="#${headingElement.id}" title="Permalink to ${headingElement.innerText}" aria-label="Permalink to ${headingElement.innerText}">${headingElement.innerHTML}</a>`;
+
+        // Optionally add icons next to the heading if the content below the heading has any images
+        if (
+            includeImages &&
+            headingElement.nextElementSibling?.classList.contains(
+                'enable-stats',
+            )
+        ) {
+            const images = findImagesNextToHeading(
+                headingElement.id,
+                'enable-stats',
+            );
+            headingElement.innerHTML = images + headingElement.innerHTML;
         }
     }
 }
@@ -53,4 +128,4 @@ function deleteCookie(name) {
     document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
 }
 
-export { scrollToEl, addMissingIDToHeading, splitCookies, getCookie, setCookie, deleteCookie }
+export { scrollToEl, focusDeepLink, addMissingIDToHeading, createPermalinksForHeading, splitCookies, getCookie, setCookie, deleteCookie }
