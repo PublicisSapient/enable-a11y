49b50acb61d7bc1cf8b7aa9222063006cc5a9da4
Alison Hall <alison.hall@publicissapient.com>
2024-06-26 11:00:00 -0400

    Improve the jscodeshift code to reattach comments

--- bin/remove-imports-exports.js
+++ bin/remove-imports-exports.js
@@ -2,16 +2,18 @@ export default function transformer(file, api) {
     const j = api.jscodeshift;
     const root = j(file.source);
 
-    const getFirstNode = () => root.find(j.Program).get('body', 0).node;
-    const getSecondNode = () => root.find(j.Program).get('body', 1).node;
+    const getFirstNodesAndComments = () => {
+        const program = root.find(j.Program);
+        const firstNode = program.get('body', 0).node;
+        const secondNode = program.get('body', 1).node;
+        const { comments: firstNodeComments } = firstNode;
+        const { comments: secondNodeComments } = secondNode;
+        return { firstNode, firstNodeComments, secondNode, secondNodeComments };
+    };
 
-    // Save the comments attached to the first node
-    const firstNode = getFirstNode();
-    const { comments } = firstNode;
-
-    // Save the comments attached to the second node
-    const secondNode = getSecondNode();
-    const { comments: comments2 } = secondNode;
+    // Save the comments attached to the first and second nodes
+    const { firstNode, firstNodeComments, secondNode, secondNodeComments } =
+        getFirstNodesAndComments();
 
     // Remove import statements
     root.find(j.ImportDeclaration).remove();
@@ -22,16 +24,14 @@ export default function transformer(file, api) {
     // Remove export default statements
     root.find(j.ExportDefaultDeclaration).remove();
 
-    // If the first node has been modified or deleted, reattach the comments
-    const firstNode2 = getFirstNode();
-    if (firstNode2 !== firstNode) {
-        firstNode2.comments = comments;
+    // If the first or second node has been modified or deleted, reattach the original comments
+    const { firstNode: newFirstNode, secondNode: newSecondNode } =
+        getFirstNodesAndComments();
+    if (newFirstNode !== firstNode) {
+        newFirstNode.comments = firstNodeComments;
     }
-
-    // If the second node has been modified or deleted, reattach the comments
-    const secondNode2 = getSecondNode();
-    if (secondNode2 !== secondNode) {
-        secondNode2.comments = comments2;
+    if (newSecondNode !== secondNode) {
+        newSecondNode.comments = secondNodeComments;
     }
 
     return root.toSource();
