6072613588047e5d3d2c65d95168edf7ce664424
Alison Hall <alison.hall@publicissapient.com>
2024-06-26 10:46:44 -0400

    Update ES4 jscodeshift script to reattach comments

--- bin/remove-imports-exports.js
+++ bin/remove-imports-exports.js
@@ -2,6 +2,17 @@ export default function transformer(file, api) {
     const j = api.jscodeshift;
     const root = j(file.source);
 
+    const getFirstNode = () => root.find(j.Program).get('body', 0).node;
+    const getSecondNode = () => root.find(j.Program).get('body', 1).node;
+
+    // Save the comments attached to the first node
+    const firstNode = getFirstNode();
+    const { comments } = firstNode;
+
+    // Save the comments attached to the second node
+    const secondNode = getSecondNode();
+    const { comments: comments2 } = secondNode;
+
     // Remove import statements
     root.find(j.ImportDeclaration).remove();
 
@@ -10,6 +21,18 @@ export default function transformer(file, api) {
 
     // Remove export default statements
     root.find(j.ExportDefaultDeclaration).remove();
+
+    // If the first node has been modified or deleted, reattach the comments
+    const firstNode2 = getFirstNode();
+    if (firstNode2 !== firstNode) {
+        firstNode2.comments = comments;
+    }
+
+    // If the second node has been modified or deleted, reattach the comments
+    const secondNode2 = getSecondNode();
+    if (secondNode2 !== secondNode) {
+        secondNode2.comments = comments2;
+    }
 
     return root.toSource();
 }
