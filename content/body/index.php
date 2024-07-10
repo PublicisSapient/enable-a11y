89f382055abd03c3915b33fb19d2278eda9014a9
Matt Slavin <matthew.slavin@publicissapient.com>
2024-07-10 16:36:08 -0400

    Update index style and content

--- content/body/index.php
+++ content/body/index.php
@@ -3,7 +3,7 @@
     <div class="homepage__hero--copy">
       <h1 class="homepage__hero--heading"><?= $title ?></h1>
 
-      <p>A place to learn and share with developers what makes web work accessible.</p>
+      <p>A space for developers to learn and collaborate on making the web accessible.</p>
     </div>
 
     <!--
@@ -26,26 +26,19 @@
 
       <h2 class="homepage__intro--header homepage__intro--header--code">Learn How To Create Accessible Code</h2>
       <p>
-        Whether you are new to accessibility or are a seasoned veteran, Enable's aim to <strong>teach developers how to
-          make
-          something accessible</strong> and to <strong>show why it is.</strong> Developers can walk through the code
-        step
-        by step to understand what HTML, CSS and JavaScript are needed to ensure their work "just works" for all users,
-        no
-        matter how they navigate the web.
+        <!-- #TODO - Rewrite this ::: -->
+        Whether you are new to accessibility or are a seasoned veteran, Enable's aim is to <strong>teach developers how to make something accessible</strong> and to <strong>show why it is.</strong> 
+        <!--Developers can walk through the code step by step to understand what HTML, CSS and JavaScript are needed to ensure their work "just works" for all users, no matter how they navigate the web. -->
       </p>
     </div>
     <div class="homepage__intro--section">
       <h2 class="homepage__intro--header homepage__intro--header--existing">Add Accessible Code Quickly To Existing
         Projects</h2>
-
       <p>
-        The Enable Accessibility Project follows the <a href="https://www.w3.org/TR/using-aria/#rule1">First Rule of
-          ARIA</a> when recommending code and encourages the use of native HTML5 functionality whenever possible (as
-        long
-        as browser accessibility support is there). However, since we know there are a lot of developers looking to make
+        The Enable Accessibility Project follows the <a href="https://www.w3.org/TR/using-aria/#rule1">First Rule of ARIA</a> when recommending code and encourages the use of native HTML5 functionality whenever possible (as
+        long as browser accessibility support is there). <!-- However, since we know there are a lot of developers looking to make
         their existing custom widgets accessible quickly due to time constraints, we also show custom code examples and
-        what code features make them work with assistive technology.
+        what code features make them work with assistive technology. -->
       </p>
 
 
@@ -54,20 +47,19 @@
       <h2 class="homepage__intro--header homepage__intro--header--npm">Use Pre-built Accessible Components</h2>
 
       <p>
-        Enable also includes components that can be included into your own projects via NPM, with instructions on how to
-        integrate them as Webpack modules, native ES6 Modules or as old school ES4 JavaScript libraries. Accessible
+        Enable also includes <a href="#">components</a> that can be included into your own projects via NPM, with instructions on how to
+        integrate them as Webpack modules, native ES6 Modules or as old school ES4 JavaScript libraries.<!-- Accessible
         hamburger menus, carousels and mobile-friendly skip links, are only a few examples of prebuilt code you can use
         in
         your projects today. We also encourage developers to consider contributing their own code for the benefit
         of
-        the developer community at large.
+        the developer community at large. -->
     </div>
   </div>
 </div>
 
 <div class="homepage__bottom-copy">
   <h2 class="homepage__bottom-copy--heading homepage__intro--header homepage__intro--header--question">What Makes Enable Different?</h2>
-
   <ul>
     <li>
       We strive to keep our <strong>codebase up-to-date</strong>, with a <strong>focus on innovation</strong> (some of
@@ -86,21 +78,14 @@
       usable by everyone.
     </li>
     <li>
-      Not only do we want to help developers, but <strong>we want the help from
-        developers to contribute their
-        accessible code.</strong>
-      If you have an accessible component that you don't see here, please consider
-      donating by raising a pull
-      request.
+      Not only do we want to help developers, but <strong>we want the help from developers to contribute their accessible code.</strong>
+      If you have a component that you'd like to submit - please put in a <!-- #TODO --> <a href="#">pr</a>
     </li>
   </ul>
 </div>
-<div class="homepage__bottom-copy">
-
+<div class="homepage__bottom-copy background-secondary">
   <h2 class="homepage__bottom-copy--heading homepage__intro--header homepage__intro--header--donate">Want to donate code to Enable?</h2>
-
   <p>We would love to know about any code you would like to donate to enable!</p>
-
   <ul>
     <li> If you would like to discuss this with us first,
       please go to the <a href="https://github.com/PublicisSapient/enable-a11y/issues">Enable Github issue page</a> and
@@ -111,10 +96,8 @@
         Github article about creating a pull request from a fork</a> for more information on how to do this.</li>
   </ul>
 </div>
-<div class="homepage__bottom-copy">
-
+<div class="homepage__bottom-copy background-tertiary">
   <h2 class="homepage__bottom-copy--heading homepage__intro--header homepage__intro--header--bug">Found a bug?</h2>
-
   <p>
     Feel free to log a bug on our Github project page! If are a developer and know how to fix it, please create a PR (if
     you have never done this before, read the <a
