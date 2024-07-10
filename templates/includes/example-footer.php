bf03c0e522c0158df360eae721fc8dc47267d8ad
Matt Slavin <matthew.slavin@publicissapient.com>
2024-07-10 16:58:39 -0400

    Update footer and logo-link

--- templates/includes/example-footer.php
+++ templates/includes/example-footer.php
@@ -1,8 +1,12 @@
-    <footer aria-label="Copyright Information">
-        <?php include "includes/about-enable-nav.php"; ?>   
+    <footer aria-label="Copyright Information" class="footer">
+        <ul class="">
+            <?php include "includes/about-enable-nav.php"; ?> 
+        </ul>
+       <div>   
         Enable is a labour of love created by
         <a href="https://useragentman.com">Zoltan Hawryluk</a>.  It has been released as open
-        source so hopefully others can learn from it.  This content is covered by the the <a href="https://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International Licence</a>.
+        source so hopefully others can learn from it.  This content is covered by the <a href="https://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International Licence</a>.
+        </div>
 
     </footer> 
         
--- templates/includes/logo-link.php
+++ templates/includes/logo-link.php
@@ -1,5 +1,9 @@
+<!-- 
+ #TODO - logo and banner text have been moved from this component. 
+ Leaving component in place for now but can possible be removed in the future. -->
 <div class="enable-logo__container">
-    <p>Banner text could go here</p>
+    
+    <!-- <p>Banner text could go here</p> -->
     <!-- <a id="enable-logo" class="enable-logo__link" href="index.php">
         <img class="enable-logo" src="images/ENABLE.svg" alt="" role="presentation" />
         <span class="enable-logo__text">Enable</span>
