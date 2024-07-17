3aa5997ae3c33bde3635bba06f79c78986088494
Jadan Ou <17204721+ocjadan@users.noreply.github.com>
2024-07-17 08:45:03 -0700

    Cookie banner message announcing for Safari and Chrome. Not Firefox. CSS styling complete.

--- content/body/cookiebanner.php
+++ content/body/cookiebanner.php
@@ -4,16 +4,30 @@
 
 <button id="show-non-modal-button">Show Non-Modal Banner</button>
 
-<dialog id="modal-cookie-banner">
-  <p>Greetings, you must accept all cookies.</p>
+<dialog id="modal-cookie-banner" class="cookie-banner" aria-describedby="modal-cookie-banner-message">
+  <p id="modal-cookie-banner-message">
+    We use strictly necessary cookies to make our Sites work. In addition, if you consent, we will use optional functional, performance and targeting cookies to help us understand how people use our website, to improve your user experience and to provide you with targeted advertisements. You can accept all cookies, or click to review your cookie preferences.
+  </p>
 
-  <button>Accept</button>
-  <button>Reject</button>
+  <form class="cookie-banner-buttons" method="dialog">
+    <button id="modal-cookie-banner-close-button" autofocus>Close</button>
+    <div class="cookie-banner-action-buttons">
+      <button id="modal-cookie-banner-accept-button">Accept</button>
+      <button id="modal-cookie-banner-reject-button" class="cookie-banner-reject-button">Reject</button>
+    </div>
+  </form>
 </dialog>
 
-<dialog id="non-modal-cookie-banner">
-  <p>Greetings, you must accept all cookies.</p>
+<dialog id="non-modal-cookie-banner" class="cookie-banner" aria-describedby="non-modal-cookie-banner-message">
+  <p id="non-modal-cookie-banner-message">
+    We use strictly necessary cookies to make our Sites work. In addition, if you consent, we will use optional functional, performance and targeting cookies to help us understand how people use our website, to improve your user experience and to provide you with targeted advertisements. You can accept all cookies, or click to review your cookie preferences.
+  </p>
 
-  <button>Accept</button>
-  <button>Reject</button>
+  <form class="cookie-banner-buttons" method="dialog">
+    <button id="modal-cookie-banner-close-button" autofocus>Close</button>
+    <div class="cookie-banner-action-buttons">
+      <button id="non-modal-cookie-banner-accept-button">Accept</button>
+      <button id="non-modal-cookie-banner-reject-button" class="cookie-banner-reject-button">Reject</button>
+    </div>
+  </form>
 </dialog>
--- content/head/cookiebanner.php
+++ content/head/cookiebanner.php
@@ -1,1 +1,1 @@
-
+<link id="cookiebanner-css" rel="stylesheet" type="text/css" href="css/cookiebanner.css" >
