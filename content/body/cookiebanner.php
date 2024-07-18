fee7b17ed2d9aa0b1d0a38d3792b2a737312235a
Jadan Ou <17204721+ocjadan@users.noreply.github.com>
2024-07-18 09:22:06 -0700

    Cookie banner announcing appropriately when shown as a modal or non-modal dialog.

--- content/body/cookiebanner.php
+++ content/body/cookiebanner.php
@@ -6,9 +6,9 @@
 
 <p>
   Unfortunately, many cookie banner implementations do not have the explanation interactive as text,
-  nor do they automatically announce the explanation to screen readers. Instead, screen reader users
-  are often guided immediately to one of the buttons—accept or reject—without knowing what it is they
-  are accepting or rejecting.
+  nor do they automatically announce that the user is now interacting with a cookie banner. Instead,
+  screen reader users are often guided immediately to one of the buttons—accept or reject—without
+  knowing what it is they are accepting or rejecting.
 </p>
 
 <p>
@@ -19,21 +19,27 @@
 <h2>Modal Cookie Banner</h2>
 
 <p>
-  Using a modal dialog for cookie banners causes the contents of the webpage to be unavailable—often
-  covered by a dark overlay—until the visitor of the webpage makes a decision on their cookie
-  preferences. The contents of the webpage cannot be interacted with until the modal dialog is
-  dismissed, this is true for both sighted users and screen readers.
+  Using a modal dialog for cookie banners causes the contents of the webpage to be unavailable —
+  often covered by a dark overlay — until the visitor of the webpage makes a decision on their
+  cookie preferences. The contents of the webpage cannot be interacted with until the modal dialog
+  is dismissed, this is true for both sighted users and screen readers.
 </p>
 
 <button id="show-modal-button">Show Modal Banner</button>
 
-<dialog id="modal-cookie-banner" class="cookie-banner" aria-describedby="modal-cookie-banner-message">
-  <p id="modal-cookie-banner-message">
-    We use strictly necessary cookies to make our Sites work. In addition, if you consent, we will use optional functional, performance and targeting cookies to help us understand how people use our website, to improve your user experience and to provide you with targeted advertisements. You can accept all cookies, or click to review your cookie preferences.
-  </p>
+<dialog id="modal-cookie-banner" class="cookie-banner">
+  <form class="cookie-banner-form" method="dialog" aria-labelledby="modal-cookie-banner-title">
+    <button id="cookie-banner-close" class="cookie-banner-close-button" autofocus>
+      <img class="a11y-modal__button--close-image" src="images/close-window.svg" alt="close dialog">
+    </button>
 
-  <form class="cookie-banner-buttons" method="dialog">
-    <button id="modal-cookie-banner-close-button" autofocus>Close</button>
+    <div role="document" tabindex="0">
+      <p id="modal-cookie-banner-title">Cookie Notice</p>
+      <p id="modal-cookie-banner-message">
+        We use strictly necessary cookies to make our Sites work.
+      </p>
+    </div>
+
     <div class="cookie-banner-action-buttons">
       <button id="modal-cookie-banner-accept-button">Accept</button>
       <button id="modal-cookie-banner-reject-button" class="cookie-banner-reject-button">Reject</button>
@@ -52,13 +58,19 @@
 
 <button id="show-non-modal-button">Show Non-Modal Banner</button>
 
-<dialog id="non-modal-cookie-banner" class="cookie-banner" aria-describedby="non-modal-cookie-banner-message">
-  <p id="non-modal-cookie-banner-message">
-    We use strictly necessary cookies to make our Sites work. In addition, if you consent, we will use optional functional, performance and targeting cookies to help us understand how people use our website, to improve your user experience and to provide you with targeted advertisements. You can accept all cookies, or click to review your cookie preferences.
-  </p>
+<dialog id="non-modal-cookie-banner" class="cookie-banner">
+  <form class="cookie-banner-form" method="dialog" aria-labelledby="non-modal-cookie-banner-title">
+    <button id="cookie-banner-close" class="cookie-banner-close-button" autofocus>
+      <img class="a11y-modal__button--close-image" src="images/close-window.svg" alt="close dialog">
+    </button>
 
-  <form class="cookie-banner-buttons" method="dialog">
-    <button id="modal-cookie-banner-close-button" autofocus>Close</button>
+    <div role="document" tabindex="0">
+      <p id="non-modal-cookie-banner-title">Cookie Notice</p>
+      <p id="non-modal-cookie-banner-message">
+        We use strictly necessary cookies to make our Sites work.
+      </p>
+    </div>
+
     <div class="cookie-banner-action-buttons">
       <button id="non-modal-cookie-banner-accept-button">Accept</button>
       <button id="non-modal-cookie-banner-reject-button" class="cookie-banner-reject-button">Reject</button>
