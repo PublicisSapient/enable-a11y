23799a4e860cdc735e180c9ad23baf0c4945bda4
Zoltan Hawryluk <zoltan.dulac@gmail.com>
2024-07-24 11:01:58 -0400

    Added video player figure styles in figure.less.

--- content/head/multimedia-content.php
+++ content/head/multimedia-content.php
@@ -3,39 +3,3 @@
 <link href="css/figure.css" rel="stylesheet">
 <link href="css/table.css" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="css/definition-term.css" >
-
-<style type="text/css">
-.video-figure { 
-    display: block;
-}
-
-
-.video-figure .able-timer,
-.video-figure .able-speed,
-.video-figure .able-status,
-.video-figure .able-elapsedTime,
-.video-figure .able-duration,
-.video-figure .able-right-controls span {
-    margin-top: 0px;
-    margin-bottom: 0px;
-    display: inline-block;
-    font-family: "Helvetica", "Arial", sans-serif;
-}
-
-.video-figure .video-figure__caption {
-    margin-bottom: 0;
-    display: block;
-}
-
-.video-figure .able-status-bar {
-    font-family: "Helvetica", "Arial", sans-serif;
-}
-
-.video-figure .able-status-bar {
-  height: 1.5em;
-  min-height: 1.5em;
-}
-.video-figure .able-status {
-    text-align: right;
-}
-</style>
