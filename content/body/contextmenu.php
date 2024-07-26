382b7223a066d236956366f360cbb089ea06cdd6
Jadan Ou <17204721+ocjadan@users.noreply.github.com>
2024-07-26 10:28:01 -0700

    Context menu items click listener.

---
+++ content/body/contextmenu.php
@@ -0,0 +1,13 @@
+<p>
+  Context menus are used for...
+</p>
+
+<div id="custom-context-menu" class="custom-context-menu">
+  <ul id="custom-context-menu-list" class="custom-context-menu__list">
+    <li class="custom-context-menu__list__item">Action 1</li>
+    <li class="custom-context-menu__list__item">Action 2</li>
+    <li class="custom-context-menu__list__item">Action 3</li>
+  </ul>
+</div>
+
+<a id="link-context-menu" class="link-context-menu" href="https://google.com">Google</a>
--- js/modules/contextmenu.js
+++ js/modules/contextmenu.js
@@ -1,7 +1,36 @@
 'use-strict'
 
 export default function ContextMenu() {
-  this.init = new function() {
-    console.log(`[ContextMenu] We have init-off!`);
+  this.init = new function () {
+    const menu = document.getElementById("custom-context-menu-list");
+    for (let child of menu.children) {
+      if (child.nodeName === "LI") {
+        child.addEventListener("click", onContextMenuItemClicked)
+      }
+    }
+    
+    document.addEventListener("contextmenu", (event) => {
+      const link = event.target.closest(".link-context-menu");
+      if (link) {
+        event.preventDefault();
+
+        const menu = document.getElementById("custom-context-menu");
+        menu.style.display = 'block';
+        menu.style.left = `${event.clientX}px`;
+        menu.style.top = `${event.clientY}px`;
+        document.addEventListener('click', hideCustomMenu);
+      }
+    })
+  }
+
+  function hideCustomMenu() {
+    const menu = document.getElementById('custom-context-menu');
+    menu.style.display = 'none';
+    document.removeEventListener('click', hideCustomMenu);
+  }
+  
+  function onContextMenuItemClicked(event) {
+    const { target } = event;
+    console.log(target.textContent);
   }
 }
