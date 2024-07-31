5c82c30a824745e6aea92a18ae9bb6d9392327ad
Jadan Ou <17204721+ocjadan@users.noreply.github.com>
2024-07-31 09:45:51 -0700

    Cookie banner built with <aside> tag.

--- content/body/cookiebanner.php
+++ content/body/cookiebanner.php
@@ -27,27 +27,15 @@
 
 <button id="show-modal-button" aria-haspopup="dialog">Show Modal Banner</button>
 
-<h2>Non-Modal Cookie Banner</h2>
-
-<p>
-  Using a non-modal dialog for cookie banners allows the contents of the webpage to still be interactive. However, focus
-  often immediately shifts to the action buttons—accept or reject—without first announcing the contents of the dialog.
-  The implementation below announces the contents while also automatically focuses on the action buttons.
-</p>
-
-<br />
-
-<button id="show-non-modal-button" aria-haspopup="dialog">Show Non-Modal Banner</button>
-
 <div id="cookie-banner-container">
   <dialog id="cookie-banner" class="cookie-banner">
-    <form class="cookie-banner__form" method="dialog" aria-labelledby="cookie-banner-title">
+    <form method="dialog" aria-labelledby="cookie-banner-title">
       <button id="cookie-banner-close-button" class="cookie-banner__close-button" autofocus>
-        <img class="cookie-banner__close-button__icon" src="images/close-window.svg" alt="close dialog">
+        <img class="cookie-banner__close-button__icon" src="images/close-window.svg" alt="close cookie notice">
       </button>
 
       <div role="document" tabindex="0">
-        <h2 id="cookie-banner-title" class="cookie-banner__form__title">Cookie Notice</h2>
+        <h2 id="cookie-banner-title" class="cookie-banner__title">Cookie Notice</h2>
         <p id="cookie-banner-message">
           We use strictly necessary cookies to make our Sites work. In addition, if you consent, we will use optional
           functional, performance and targeting cookies to help us understand how people use our website, to improve your
@@ -67,22 +55,70 @@
 <?php includeShowcode("cookie-banner-container"); ?>
 
 <script type="application/json" id="cookie-banner-container-props">
-{
-  "replaceHtmlRules": {},
-  "steps": [
-    {
-      "label": "Use a form and ensure it has its aria-labelledby attribute set",
-      "highlight": "aria-labelledby",
-      "notes": "Using a form is good practice—<a href=\"https://developer.mozilla.org/en-US/docs/Web/HTML/Element/dialog#usage_notes\">MDN docs</a>—to have the dialog close automatically when a button within it is pressed. Using aria-labelledby along with using a div element with a role of document (next step) ensures it's read out when the dialog is opened."
-    },
-    {
-      "label": "Use the document role to enclose the cookie explanation",
-      "highlight": "role",
-      "notes": "This presents the content in reading mode for screen readers."
-    }
-  ]
-}
+  {
+    "replaceHtmlRules": {},
+    "steps": [
+      {
+        "label": "Use the HTML dialog tag",
+        "highlight": "%OPENCLOSECONTENTTAG%dialog",
+        "notes": "The dialog tag comes with handy functionality such as a <code>.showModal()</code> method."
+      },
+      {
+        "label": "Use a form to encapsulate the contents of the cookie banner",
+        "highlight": "%OPENCLOSECONTENTTAG%form",
+        "notes": "Using a form is good practice—<a href=\"https://developer.mozilla.org/en-US/docs/Web/HTML/Element/dialog#usage_notes\">MDN docs</a>—to have the dialog close automatically when a button within it is pressed."
+      },
+      {
+        "label": "Ensure the form element has \"aria-labelledby\" set",
+        "highlight": "%INLINE%aria-labelledby=\"cookie-banner-title\"",
+        "notes": "Using aria-labelledby along with a div element with a role of document ensures it's read out when the dialog is opened."
+      },
+      {
+        "label": "Use the document role to enclose the cookie explanation",
+        "highlight": "%INLINE%role=\"document\"",
+        "notes": "This presents the content in reading mode for screen readers."
+      }
+    ]
+  }
 </script>
+
+<p>
+  With the HTML set up, use the built-in method <code>.showModal()</code> of the <code>dialog</code> HTML tag to show
+  the cookie banner.
+</p>
+
+<h2>Non-Modal Cookie Banner</h2>
+
+<p>
+  Using a non-modal dialog for cookie banners allows the contents of the webpage to still be interactive. However, focus
+  often immediately shifts to the action buttons—accept or reject—without first announcing the contents of the dialog.
+  The implementation below announces the contents while also automatically focuses on the action buttons.
+</p>
+
+<br />
+
+<button id="show-non-modal-button" aria-haspopup="dialog">Show Non-Modal Banner</button>
+
+<aside id="non-modal-cookie-banner" class="non-modal-cookie-banner" aria-labelledby="non-modal-cookie-banner-title">
+  <button id="non-modal-cookie-banner-close-button" class="cookie-banner__close-button" autofocus>
+    <img class="cookie-banner__close-button__icon" src="images/close-window.svg" alt="close cookie notice">
+  </button>
+
+  <div role="document" tabindex="0">
+    <h2 id="non-modal-cookie-banner-title" class="cookie-banner__title">Cookie Notice</h2>
+    <p id="non-modal-cookie-banner-message">
+      We use strictly necessary cookies to make our Sites work. In addition, if you consent, we will use optional
+      functional, performance and targeting cookies to help us understand how people use our website, to improve your
+      user experience and to provide you with targeted advertisements. You can accept all cookies, or click to review
+      your cookie preferences.
+    </p>
+  </div>
+
+  <div class="cookie-banner__action-buttons">
+    <button id="non-modal-cookie-banner-accept-button" class="cookie-banner__accept-button">Accept</button>
+    <button id="non-modal-cookie-banner-reject-button" class="cookie-banner__reject-button">Reject</button>
+  </div>
+</aside>
 
 <p>
   With the HTML set up, use the built-in methods <code>.showModal()</code> or <code>.show()</code> for the
--- js/modules/es4/cookiebanner.js
+++ js/modules/es4/cookiebanner.js
@@ -2,11 +2,12 @@
 
 const cookieBanner = new (function() {
   let cookieBanner;
+  let cookieBannerAside;
 
   this.init = function() {
     cookieBanner = document.getElementById('cookie-banner');
     setUpModalButton();
-    setUpNonModalButton();
+    setUpNonModalCookieBanner();
   }
 
   function setUpModalButton() {
@@ -15,11 +16,32 @@ const cookieBanner = new (function() {
       cookieBanner.showModal();
     });
   }
+  
+  function setUpNonModalCookieBanner() {
+    cookieBannerAside = document.getElementById('non-modal-cookie-banner');
+    setUpShowNonModalButton();
+    setUpNonModalActionButtons();
+  }
 
-  function setUpNonModalButton() {
+  function setUpShowNonModalButton() {
     const showNonModalButton = document.getElementById('show-non-modal-button');
-    showNonModalButton.addEventListener('click', () => {
-      cookieBanner.show();
+    showNonModalButton.addEventListener('click', (event) => {
+      cookieBannerAside.classList.add('non-modal-cookie-banner__show');
     })
+  }
+  
+  function setUpNonModalActionButtons() {
+    const close = document.getElementById('non-modal-cookie-banner-close-button');
+    close.addEventListener('click', onNonModalActionButtonClicked);
+    
+    const accept = document.getElementById('non-modal-cookie-banner-accept-button');
+    accept.addEventListener('click', onNonModalActionButtonClicked);
+    
+    const reject = document.getElementById('non-modal-cookie-banner-reject-button')
+    reject.addEventListener('click', onNonModalActionButtonClicked);
+  }
+  
+  function onNonModalActionButtonClicked() {
+    cookieBannerAside.classList.remove('non-modal-cookie-banner__show');
   }
 })
