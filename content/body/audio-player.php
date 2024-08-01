f710d3541e010006ed8de2cebfbb2db93edadabf
Ankita Agarwal <ankagarw17@WKMZTC28B553.global.publicisgroupe.net>
2024-08-01 14:17:24 -0400

    used whisper and ffmpeg to generate subtitles for audio file

--- content/body/audio-player.php
+++ content/body/audio-player.php
@@ -21,8 +21,8 @@
 <div id="example1">
   <div class="enable-media-player">
     <audio id="audio1" preload="auto" data-able-player data-skin="2020" data-root-path="./js/enable-libs/ableplayer/" data-heading-level="3">
-      <source type="audio/mpeg" src="audio/example.mp3" />
-      <track kind="captions" src="vtt/dialog-document__html5.vtt" srclang="en" label="English Captions" />
+      <source type="audio/mpeg" src="audio/audio.mp3" />
+      <track kind="captions" src="vtt/audio.vtt" srclang="en" label="English Captions" />
     </audio>
   </div>
 </div>
