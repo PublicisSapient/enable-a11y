05bef7c3f13b14a0064cbc2d594424a9c2008951
Zoltan Hawryluk <zoltan.dulac@gmail.com>
2024-10-28 14:56:40 -0400

    Fix HTML error.

--- templates/includes/section-page-templates.php
+++ templates/includes/section-page-templates.php
@@ -31,7 +31,7 @@
 <template id="page-list-item">
     <a href="/${slug}.php" class="item-wrapper">
         <div class="icon-wrapper">
-            <h3 class="page-list__title id="${slug}__page" >${visibleTitle}</h3>
+            <h3 class="page-list__title" id="${slug}__page" >${visibleTitle}</h3>
             <div class="icon">
                 <img src="/images/icons/${page}/${slug}.${thumbFileType}" alt="" role="presentation">
             </div>
