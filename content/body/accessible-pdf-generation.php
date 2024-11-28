f4fcb9720aae4ec8b5de21bcbe3d0977e9e0c110
Sam Cattani <samantha.cattani_ext@sonepar.com>
2024-11-28 13:41:39 -0500

    Changed <> to &lt;&gt;

--- content/body/accessible-pdf-generation.php
+++ content/body/accessible-pdf-generation.php
@@ -265,7 +265,7 @@ function base64ToArrayBuffer(base64) {
     var binaryString = window.atob(base64);
     var binaryLen = binaryString.length;
     var bytes = new Uint8Array(binaryLen);
-    for (var i = 0; i < binaryLen; i++) {
+    for (var i = 0; i &lt; binaryLen; i++) {
       var ascii = binaryString.charCodeAt(i);
       bytes[i] = ascii;
     }
