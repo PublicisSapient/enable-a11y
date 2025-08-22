d392c89d15021a0faccc445203a217e72c3f4c52
Zoltan Hawryluk <zoltan.dulac@gmail.com>
2025-08-22 13:47:57 -0400

    Adding "radio buttons that don't look like radio buttons" example

--- content/body/focus-styling.php
+++ content/body/focus-styling.php
@@ -22,33 +22,41 @@ We need to handle this:
 
 
 <p>
-  Before we dive into making focus outlines accessible, let's first understand what a focus outline is and why it’s important for web accessibility, particularly for screen readers. 
-  If you're already familiar with focus outlines, feel free to skip to the next section. For those who aren’t, here's a brief definition.
+  Before we dive into making focus outlines accessible, let's first understand what a focus outline is and why it’s
+  important for web accessibility, particularly for screen readers.
+  If you're already familiar with focus outlines, feel free to skip to the next section. For those who aren’t, here's a
+  brief definition.
 </p>
 
 <h2>What is a Focus Outline?</h2>
 
 <p>
-  Simply put, when a specific HTML element is ready to accept keyboard input, it is considered to be "in focus." 
-  As users navigate a webpage using the keyboard (typically via the Tab key), different elements receive focus, allowing screen readers to read their content. 
-  According to accessibility standards, any element in focus should have a visible border around it, known as the focus outline. This helps users, especially those relying on screen readers or keyboard navigation, to know which element is currently active.
+  Simply put, when a specific HTML element is ready to accept keyboard input, it is considered to be "in focus."
+  As users navigate a webpage using the keyboard (typically via the Tab key), different elements receive focus, allowing
+  screen readers to read their content.
+  According to accessibility standards, any element in focus should have a visible border around it, known as the focus
+  outline. This helps users, especially those relying on screen readers or keyboard navigation, to know which element is
+  currently active.
 </p>
 
 
 <p>
-  Focus states are used by keyboard users to know what interactive element they can currently manipulate. They are easily
-  styled with the <code>outline</code> CSS property and the <code>:focus</code> and <code>:focus-visible</code> pseudo-classes.
-  Unfortunately, many designers hate them and try to get focus-styles removed from a website.  This page discussed 
-  in detail why this should be avoided and how to keep designers happy by having focus-styles appear for keyboard users only. 
+  Focus states are used by keyboard users to know what interactive element they can currently manipulate. They are
+  easily
+  styled with the <code>outline</code> CSS property and the <code>:focus</code> and <code>:focus-visible</code>
+  pseudo-classes.
+  Unfortunately, many designers hate them and try to get focus-styles removed from a website. This page discussed
+  in detail why this should be avoided and how to keep designers happy by having focus-styles appear for keyboard users
+  only.
 </p>
 
 <h2>Focus Styling For Keyboard Users Only</h2>
 
 
 <?php includeStats([
-    "isForNewBuilds" => true,
-    "comment" =>
-        "This is recommended for use in both new and existing projects.  It ",
+  "isForNewBuilds" => true,
+  "comment" =>
+    "This is recommended for use in both new and existing projects.  It ",
 ]); ?>
 
 
@@ -139,29 +147,33 @@ We need to handle this:
 <h2>Increase Hit Areas Inside Focusable Elements</h2>
 
 <p>
-  If you use a keyboard to navigate through the main navigation, 
-  you will notice the clickable hit area of the top-level navigation items are a lot bigger than they take up in the layout:
+  If you use a keyboard to navigate through the main navigation,
+  you will notice the clickable hit area of the top-level navigation items are a lot bigger than they take up in the
+  layout:
 </p>
 
 
 <figure class="wide centered-image">
 
   <?php pictureWebpPng(
-      "images/focus/clickable-hit-state",
-      "Screenshot of the Enable website's main navigation, with keyboard focus applied to the 'controls' navigation drawer.",
+    "images/focus/clickable-hit-state",
+    "Screenshot of the Enable website's main navigation, with keyboard focus applied to the 'controls' navigation drawer.",
   ); ?>
 
   <figcaption>Figure 3. The focus state of the "Controls" navigation button. Note the large hit area.</figcaption>
 </figure>
 
 <p>We increased the hit area to conform to <a href="https://www.w3.org/WAI/WCAG21/Understanding/target-size.html">WCAG
-    2.5.5: Target Size</a> (we made it larger than 44 pixels x 44 pixels). Even though this is a AAA requirement, it is so easy to implement by increasing the padding
-  and compensating visually with an equivalent negative margin, so why just conform to 
-  <a href="https://www.w3.org/WAI/WCAG22/Understanding/target-size-minimum.html">WCAG 2.5.8: Target Size (Minimum).</a> (which only asks 24 pixels x 24 pixels)?</p>
+    2.5.5: Target Size</a> (we made it larger than 44 pixels x 44 pixels). Even though this is a AAA requirement, it is
+  so easy to implement by increasing the padding
+  and compensating visually with an equivalent negative margin, so why just conform to
+  <a href="https://www.w3.org/WAI/WCAG22/Understanding/target-size-minimum.html">WCAG 2.5.8: Target Size (Minimum).</a>
+  (which only asks 24 pixels x 24 pixels)?
+</p>
 
-  <figure class="wide centered-image">
-<?php includeShowcode("css-focus-hitarea", "", "", "", false); ?>
-<script type="application/json" id="css-focus-hitarea-props">
+<figure class="wide centered-image">
+  <?php includeShowcode("css-focus-hitarea", "", "", "", false); ?>
+  <script type="application/json" id="css-focus-hitarea-props">
 {
   "replaceHtmlRules": {},
   "steps": [{
@@ -171,7 +183,7 @@ We need to handle this:
   }]
 }
 </script>
-<figcaption>Figure 3a. Code pattern that increases the hit area around navigation buttons.</figcaption>
+  <figcaption>Figure 3a. Code pattern that increases the hit area around navigation buttons.</figcaption>
 
 </figure>
 
@@ -321,7 +333,7 @@ instead of using <code>outline: none</code> to remove the default focus ring, de
 
 <figure class="wide centered-image">
   <table class="screenshot-table" aria-labelledby="double-focus-screenshot-table-caption">
-    
+
     <thead>
       <tr>
         <th scope="col">
@@ -337,9 +349,9 @@ instead of using <code>outline: none</code> to remove the default focus ring, de
         <th scope="row">No Element is Focused</th>
         <td>
           <?php pictureWebpPng(
-              "images/focus/double-focus-ring__initial-state",
-              "Two yellow blocky interactive elements on a gradient background. The gradient is starts on a light yellow on the left and ends with a darker red on the right.",
-              "",
+            "images/focus/double-focus-ring__initial-state",
+            "Two yellow blocky interactive elements on a gradient background. The gradient is starts on a light yellow on the left and ends with a darker red on the right.",
+            "",
           ); ?>
         </td>
       </tr>
@@ -347,9 +359,9 @@ instead of using <code>outline: none</code> to remove the default focus ring, de
         <th scope="row">Focus on Lighter Area of Gradient</th>
         <td>
           <?php pictureWebpPng(
-              "images/focus/double-focus-ring__light-bg",
-              "The same interactive elements on the same gradient background.  The interactive element on the left is focused, and the blue focus outline around it is easily seen in contrast with the light background.",
-              "",
+            "images/focus/double-focus-ring__light-bg",
+            "The same interactive elements on the same gradient background.  The interactive element on the left is focused, and the blue focus outline around it is easily seen in contrast with the light background.",
+            "",
           ); ?>
         </td>
       </tr>
@@ -357,9 +369,9 @@ instead of using <code>outline: none</code> to remove the default focus ring, de
         <th scope="row">Focus on Darker Area of Gradient</th>
         <td>
           <?php pictureWebpPng(
-              "images/focus/double-focus-ring__darker-bg",
-              "The same interactive elements on the same gradient background.  The interactive element on the right is now focused, and the white box shadow that appears outside the darker blue focus outline ensures the focus ring has enough contrast with the dark background.",
-              "",
+            "images/focus/double-focus-ring__darker-bg",
+            "The same interactive elements on the same gradient background.  The interactive element on the right is now focused, and the white box shadow that appears outside the darker blue focus outline ensures the focus ring has enough contrast with the dark background.",
+            "",
           ); ?>
         </td>
 
@@ -367,10 +379,14 @@ instead of using <code>outline: none</code> to remove the default focus ring, de
     </tbody>
   </table>
 
-  <figcaption id="double-focus-screenshot-table-caption">Figure 7. Dual-Colored Focus States on a Gradient Background</figcaption>
+  <figcaption id="double-focus-screenshot-table-caption">Figure 7. Dual-Colored Focus States on a Gradient Background
+  </figcaption>
 </figure>
 
-<p>Here is the markup that implements the double focus ring.  Notice the use of both <code>outline</code> and <code>box-shadow</code> to create this effect (the box-shadow offsets must be greater than the outline thickness in order for this to work):</p>
+<p>Here is the markup that implements the double focus ring. Notice the use of both <code>outline</code> and
+  <code>box-shadow</code> to create this effect (the box-shadow offsets must be greater than the outline thickness in
+  order for this to work):
+</p>
 
 <?php includeShowcode("double-focus-ring-example", "", "", "", false); ?>
 <script type="application/json" id="double-focus-ring-example-props">
@@ -388,21 +404,111 @@ instead of using <code>outline: none</code> to remove the default focus ring, de
 
 <h2 id="kb-only-focus-within">Showing Instructions for Keyboard Users Only</h2>
 
+<p>
+  Keyboard users sometimes need cues that mouse users don’t. For example, when navigating a group of radio buttons,
+  sighted keyboard users usually recognize the familiar circles and know from experience that they must use the Arrow
+  keys to move between choices. Screen reader users also get this cue, since the controls are announced as a group of
+  radio buttons.</p>
+
+
+<figure class="wide centered-image">
+
+
+
+  <img src="images/focus/radio-button-example.webp" alt="" />
+
+
+
+
+
+  <figcaption>Figure 8a: A typical radio button group. Note the circle next to each radio button label, which is filled
+    when the radio button is selected.
+  </figcaption>
+</figure>
+
+<p>Problems arise when a designer restyles radio buttons to look like segmented buttons, chips, or tiles. Mouse users
+  can click around, experiment, and figure it out, but keyboard-only users may get stuck wondering why Tab seems to skip
+  past the options. Without the visual circles, it’s no longer obvious that Arrow keys (left/right or up/down) are
+  required to navigate between choices..</p>
+
+<figure class="wide centered-image">
+
+
+
+  <img src="images/focus/custom-radio-button-example.webp" alt="" />
+
+
+
+
+
+
+  <figcaption>Figure 8b: A radio button group where the radio buttons don't have the circles next to them.
+  </figcaption>
+</figure>
+
+<p>The solution is to give keyboard users just-in-time instructions when they move focus into the component. These
+  instructions should only appear during keyboard navigation, never for mouse users, since they don’t need them. Try it
+  below: use your keyboard to focus on one of the radio buttons and you’ll see how the instructions appear exactly when
+  they’re needed.</p>
+
 <div id="kb-only-focus-within__example" class="enable-example">
 
   <form>
-    <fieldset class="kb-instructions__container">
+    <fieldset>
       <legend>Product Size</legend>
 
 
-      <p>How much Zoltania Olive Oil would you like to buy?</p>
+      <p>How much <a href="javascript:alert('There is no such thing as Zoltania Olive Oil. This link is just here so sighted users can see how the instructions will appear visually when the user tabs into the radio buttons below');">Zoltania Olive Oil</a> would you like to buy?</p>
 
-      <div class="kb-instructions__text">Use arrow keys to choose one of the following</div>
-      <div class="circleless-radio-buttons"><input name="radio-group-01" type="radio" id="radio-a1"><label for="radio-a1">100 mL</label></div>
-      <div class="circleless-radio-buttons"><input name="radio-group-01" type="radio" id="radio-a2"><label for="radio-a2">500 mL</label></div>
-      <div class="circleless-radio-buttons"><input name="radio-group-01" type="radio" id="radio-a3"><label for="radio-a3">1 L</label></div>
-      <div class="circleless-radio-buttons"><input name="radio-group-01" type="radio" id="radio-a4"><label for="radio-a4">10 L</label></div>
-      <div class="circleless-radio-buttons"><input name="radio-group-01" type="radio" id="radio-a5"><label for="radio-a5">More than I'm ever going to use</label></div>
+      <div  class="has-kb-instructions__container">
+        <div id="kb-instructions__radio" class="sr-only has-kb-instructions__text">Use arrow keys to choose one of the options.</div>
+
+        <div class="circleless-radio-buttons"><input name="radio-group-01" type="radio" id="radio-a1" aria-describedby="kb-instructions__radio"><label
+            for="radio-a1">100 mL</label></div>
+        <div class="circleless-radio-buttons"><input name="radio-group-01" type="radio" id="radio-a2" aria-describedby="kb-instructions__radio"><label
+            for="radio-a2">500 mL</label></div>
+        <div class="circleless-radio-buttons"><input name="radio-group-01" type="radio" id="radio-a3" aria-describedby="kb-instructions__radio"><label
+            for="radio-a3">1 L</label></div>
+        <div class="circleless-radio-buttons"><input name="radio-group-01" type="radio" id="radio-a4" aria-describedby="kb-instructions__radio"><label
+            for="radio-a4">10 L</label></div>
+        <div class="circleless-radio-buttons"><input name="radio-group-01" type="radio" id="radio-a5" aria-describedby="kb-instructions__radio"><label
+            for="radio-a5">More than I'm ever going to use</label></div>
+      </div>
     </fieldset>
+
+    <button class="kb-only-focus-within__example__button" type="submit" onclick="javascript:('This is just an example form. There is no such thing as Zoltania Olive Oil.  Sorry to disappoint you.">Submit</button>
   </form>
 </div>
+
+<p>
+  Below is the code walkthrough of the above example:
+</p>
+
+<?php includeShowcode("kb-only-focus-within__example", "", "", "", true); ?>
+<script type="application/json" id="kb-only-focus-within__example-props">
+{
+  "replaceHtmlRules": {
+  },
+  "steps": [
+  {
+    "label": "Mark up a semantic radio group", 
+    "highlight": "%OPENCLOSETAG%fieldset ||| %OPENCLOSECONTENTTAG%legend ||| %OPENCLOSETAG%input",
+    "notes": "As mentioned in <a href='radiogroup.php'>Enable Radiogroup page</a>, use a fieldset and legend to name the group, and use native radio buttons with associated label tags."
+  },
+  {
+    "label": "Mark up the keyboard instructions", 
+    "highlight": "%OPENCLOSECONTENTTAG%div id=\"kb-instructions__radio\"",
+    "notes": "Note the sr-only class, which will make the instructions hidden by default to sighted users."
+  },
+  {
+    "label": "Attach the instructions to every radio button using aria-describedby",
+    "highlight": "aria-describedby",
+    "notes": "This is so when screen reader users access the radio buttons, the instructions are given to them."
+  },
+  {
+    "label": "CSS to ensure that when focus is inside the radio group, the instructions appear",
+    "highlight": "%CSS% focus-styling-css~ .has-kb-instructions__text ||| %CSS% focus-styling-css~ .has-kb-instructions__container:has(:focus-visible):focus-within .has-kb-instructions__text",
+    "notes": "The first bit of CSS is the visual style of the instructions.  The second bit basically tells the browser \"When focus is inside the element with class has-kb-instructions__container, make the instructions visible to sighted users.  It does this by undoing everything in <a href=\"http://localhost:8888/screen-reader-only-text.php\">the sr-only class</code>."
+  }
+]}
+</script>
